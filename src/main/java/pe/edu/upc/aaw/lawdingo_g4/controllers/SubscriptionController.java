f5bf9531cc4d6b5dabaa0f6f6f85c60c8aea0597
Nicolas Carrasco <u202116532@upc.edu.pe>
2023-09-24 21:19:20 -0500

    Repository copy upload

---
+++ src/main/java/pe/edu/upc/aaw/lawdingo_g4/controllers/SubscriptionController.java
@@ -0,0 +1,74 @@
+package pe.edu.upc.aaw.lawdingo_g4.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.SubscriptionDTO;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.UserDTO;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.UsersBySubscriptionDTO;
+import pe.edu.upc.aaw.lawdingo_g4.entities.Subscription;
+import pe.edu.upc.aaw.lawdingo_g4.entities.Users;
+import pe.edu.upc.aaw.lawdingo_g4.serviceinterfaces.ISubscriptionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/subscription")
+public class SubscriptionController {
+
+
+    @Autowired
+    private ISubscriptionService uS;
+
+    @PostMapping
+    public void create(@RequestBody SubscriptionDTO dto){
+        ModelMapper m = new ModelMapper();
+        Subscription c =m.map(dto,Subscription.class);
+        uS.create(c);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
+    public List<SubscriptionDTO> list() {
+        return uS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x, SubscriptionDTO.class);
+
+        }).collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/cantidausuarioporsuscripcion")
+    public List<UsersBySubscriptionDTO>cantidausuarioporsuscripcion (){
+        List<String[]> lista = uS.querieSubscription();
+        List<UsersBySubscriptionDTO> listDTO = new ArrayList<>();
+        for(String[] data : lista){
+            UsersBySubscriptionDTO dto = new UsersBySubscriptionDTO();
+            dto.setNameSubscription(data[0]);
+            dto.setCountUsers(Integer.parseInt(data[1]));
+            listDTO.add(dto);
+
+        }
+        return listDTO;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        uS.delete(id);
+    }
+    @PutMapping()
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void goUpdate(@RequestBody SubscriptionDTO dto){
+        ModelMapper m=new ModelMapper();
+        Subscription u=m.map(dto,Subscription.class);
+        uS.create(u);
+    }
+
+
+
+
+}
---
+++ src/main/java/pe/edu/upc/aaw/lawdingo_g4/controllers/UserController.java
@@ -0,0 +1,79 @@
+package pe.edu.upc.aaw.lawdingo_g4.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.CategoryDTO;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.CourtDTO;
+import pe.edu.upc.aaw.lawdingo_g4.dtos.UserDTO;
+import pe.edu.upc.aaw.lawdingo_g4.entities.Category;
+import pe.edu.upc.aaw.lawdingo_g4.entities.Users;
+import pe.edu.upc.aaw.lawdingo_g4.serviceinterfaces.IUserService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private PasswordEncoder bcrypt;
+    @Autowired
+    private IUserService uS;
+
+
+    @PostMapping("/save")
+    public String saveUser(@Valid Users user, BindingResult result, Model model, SessionStatus status, @RequestBody UserDTO dto)
+            throws Exception {
+        if (result.hasErrors()) {
+            return "usersecurity/user";
+        } else {
+
+            ModelMapper m = new ModelMapper();
+            Users u = m.map(dto, Users.class);
+            String bcryptPassword = bcrypt.encode(u.getPassword());
+            u.setPassword(bcryptPassword);
+            uS.insert(u);
+            return "Usuario creado";
+        }
+
+    }
+
+    //PROBAR
+
+    @GetMapping("/startsWith/{letter}")
+    public List<Users> getUsersWhoseNameStartsWith(@PathVariable String letter) {
+        return uS.getUsersWhoseNameStartsWith(letter);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id){
+        uS.delete(id);
+    }
+
+    @GetMapping
+    public List<UserDTO> list(){
+        return uS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UserDTO.class);
+        }).collect(Collectors.toList());
+    }
+    @PutMapping()
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void goUpdate(@RequestBody UserDTO dto){
+        ModelMapper m=new ModelMapper();
+        Users u=m.map(dto,Users.class);
+        uS.insert(u);
+    }
+}
+
+
+
